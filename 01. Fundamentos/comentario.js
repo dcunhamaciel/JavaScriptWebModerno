665f8c7d49211196cc2be17a59f4a9e4adfb4784
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-16 09:49:33 -0300

    Comentários de Código

---
+++ 01. Fundamentos/comentario.js
@@ -0,0 +1,15 @@
+// Comentário de uma linha
+console.log("Linha 1");
+
+/*
+Comentário de
+múltiplas linhas
+*/
+console.log("Linha 2");
+
+/*
+ * Comentário de
+ * múltiplas linhas
+ * com "estilo"
+ */
+console.log("Linha 3");
