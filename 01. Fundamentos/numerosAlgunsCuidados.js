3cae80aa386dc427af1098c9ba63ecfc1a9f9d3f
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-16 11:12:30 -0300

    Cuidados com Number

--- 01. Fundamentos/numerosAlgunsCuidados.js
+++ 01. Fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0); // Infinity
 console.log("10" / 2); // 5
+console.log("3" + 2); // 32 (operador + prevalece a string)
+console.log("3" - 2); // 1
 console.log("Show" * 2); // NaN (not a number)
 console.log(0.1 + 0.7); // 0.7999999999999999 (especificação IEEE)
 console.log((0.1 + 0.7).toFixed(2)); // 0.80
