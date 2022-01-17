40dcb4b99b899a321b960227e07af7a549bd3d8d
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-17 17:37:28 -0300

    Funções

---
+++ 01. Fundamentos/funcao1.js
@@ -0,0 +1,18 @@
+//função sem retorno
+function imprimirSoma(a, b) {
+    console.log(a + b);
+};
+
+imprimirSoma(2, 3);
+imprimirSoma(2); //NaN
+imprimirSoma(2, 10, 4, 5, 6, 7); //12
+imprimirSoma(); //NaN
+
+//função com retorno
+function soma(a, b = 0) {
+    return a + b
+}
+
+console.log(soma(2, 3));
+console.log(soma(2));
+console.log(soma());
