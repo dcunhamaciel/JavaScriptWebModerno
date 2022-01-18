7a8d17dca83e4fa9aa3d61bcb4423250b177836d
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-17 21:19:47 -0300

    Variáveis com VAR

---
+++ 01. Fundamentos/usandoVar1.js
@@ -0,0 +1,17 @@
+{
+    {
+        {
+            {
+                var sera = "Será?";
+            }
+        }
+    }
+}
+
+console.log(sera);
+
+function teste() {
+    var local = 123;
+}
+
+teste();
