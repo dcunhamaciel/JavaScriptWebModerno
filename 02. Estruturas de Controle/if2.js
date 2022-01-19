1dd380dea4ee022b728c70497c998bf196ab2aea
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-19 19:30:42 -0300

    Estrutura IF

---
+++ 02. Estruturas de Controle/if2.js
@@ -0,0 +1,17 @@
+function teste1(num) {
+    if (num > 7)
+        console.log(num);
+        console.log("Final."); //bad indentation
+}
+
+teste1(6);
+teste1(8);
+
+function teste2(num) {
+    if (num > 7); { // cuidado com o ";"
+        console.log(num);
+    }
+}
+
+teste2(6);
+teste2(8);
