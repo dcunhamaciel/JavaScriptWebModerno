0df2a143298ce66fb2bfc1cb11e0f0166f95f516
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-20 14:08:00 -0300

    Estrutura FOR

---
+++ 02. Estruturas de Controle/for1.js
@@ -0,0 +1,9 @@
+for (let contador = 1; contador <= 10; contador++) {
+    console.log(`contador = ${contador}`);
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7];
+
+for (let nota = 0; nota < notas.length; nota++) {
+    console.log(`nota ${nota+1} = ${notas[nota]}`);    
+}
