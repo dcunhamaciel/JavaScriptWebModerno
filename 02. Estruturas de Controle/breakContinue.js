d31cbf30b2f96165534441e7a6af7c9c812f0058
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-20 14:27:43 -0300

    Usando Break/Continue

---
+++ 02. Estruturas de Controle/breakContinue.js
@@ -0,0 +1,25 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8 , 9 ,10];
+
+for (let x in nums) {
+    if (x == 5) {
+        break;
+    }
+    console.log(`x${x} = ${nums[x]}`);
+}
+
+for (let y in nums) {
+    if (y == 5) {
+        continue;
+    }
+    console.log(`x${y} = ${nums[y]}`);    
+}    
+
+externo:
+for (let a in nums) {
+    for (b in nums) {
+        if (a == 2 && b == 3) {
+            break externo;
+        }
+        console.log(`a=${a} | b=${b}`);
+    }
+}
