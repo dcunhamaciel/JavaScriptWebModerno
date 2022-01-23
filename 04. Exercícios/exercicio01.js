c36c99bb9893f618a4e6c40e045447f8741fe867
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-23 09:14:19 -0300

    Exercício 01

---
+++ 04. Exercícios/exercicio01.js
@@ -0,0 +1,8 @@
+function operacoesMatematicas(a, b) {
+    console.log(`${a} + ${b} = ${a + b}`);
+    console.log(`${a} - ${b} = ${a - b}`);
+    console.log(`${a} * ${b} = ${a * b}`);
+    console.log(`${a} / ${b} = ${a / b}`);
+}
+
+operacoesMatematicas(10, 5);
