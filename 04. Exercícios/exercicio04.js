0965cef5e1ec969bc755f87dc32d43722d278d29
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-23 09:32:30 -0300

    Exercício 04

---
+++ 04. Exercícios/exercicio04.js
@@ -0,0 +1,10 @@
+// Crie uma função que irá receber dois valores, o dividendo e o divisor.
+// A função deverá imprimir o resultado e o resto da divisão destes dois valores.
+
+function divisao(dividendo, divisor) {
+    console.log(`Divisão de ${dividendo} por ${divisor} = ${dividendo / divisor}`);
+    console.log(`Resto da divisão de ${dividendo} por ${divisor} = ${dividendo % divisor}`);
+}
+
+divisao(2, 2);
+divisao(3, 2);
