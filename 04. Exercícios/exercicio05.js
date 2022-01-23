8da4151f9c0b05daacb320fd13ad29077da695eb
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-23 09:38:55 -0300

    Exercício 05

---
+++ 04. Exercícios/exercicio05.js
@@ -0,0 +1,12 @@
+// Lidar com números em JavaScript pode dar muita dor de cabeça. Você já viu o que acontece quando faz o
+// seguinte comando no console: console.log(0.1 + 0.2); O resultado será: 0.30000000000000004. Outra coisa
+// importante de observar, é o fato que o ponto é utilizado no lugar da vírgula e vice versa. Com isso, vamos fazer
+// um exercício simples para mostrar dinheiro sempre da forma correta. Desenvolva uma função JavaScript para
+// que ela receba um valor como 0.30000000000000004 e retorne R$0,30 (observe a vírgula e o ponto).
+
+function imprimirValorEmReal(valor) {    
+    console.log("R$", valor.toFixed(2).toString().replace(".", ","));    
+}
+
+imprimirValorEmReal(0.30000000000000004);
+imprimirValorEmReal(11.3541);
