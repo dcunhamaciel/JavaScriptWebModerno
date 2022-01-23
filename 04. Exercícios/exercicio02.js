a1cebe59338a1fb541789479eb31ecfa6174355d
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-23 09:24:40 -0300

    Exercício 02

--- 04. Exercícios/exercicio02.js
+++ 04. Exercícios/exercicio02.js
@@ -1,3 +1,7 @@
+// Os triângulos podem ser classificados em 3 tipos quanto ao tamanho de seus lados:
+// Equilátero: Os três lados são iguais. Isósceles: Dois lados iguais. Escaleno: Todos os lados são diferentes.
+// Crie uma função que recebe os comprimentos dos três lados de um triângulo e retorne sua classificação quanto ao tamanho de seus lados
+
 function tipoTriangulo(lado1, lado2, lado3) {
     if (lado1 == lado2 && lado2 == lado3) {
         return "Equilátero"
