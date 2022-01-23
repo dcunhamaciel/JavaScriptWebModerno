c7377a8c98d53efcf28a0872504ddb580b6ae39a
dcunhamaciel <dcunhamaciel@gmail.com>
2022-01-23 09:23:39 -0300

    Exercício 01

--- 04. Exercícios/exercicio01.js
+++ 04. Exercícios/exercicio01.js
@@ -1,3 +1,6 @@
+// Crie uma função que dado dois valores (passados como parâmetros) mostre no console a
+// soma, subtração, multiplicação e divisão desses valores
+
 function operacoesMatematicas(a, b) {
     console.log(`${a} + ${b} = ${a + b}`);
     console.log(`${a} - ${b} = ${a - b}`);
