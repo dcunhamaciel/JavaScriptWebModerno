56cf4b0e301d5800a8b0e04b3ea7616aa2f447e7
Diego Maciel <dcunhamaciel@gmail.com>
2022-01-29 07:24:37 -0300

    Funções Importantes de Object

---
+++ 05. Objeto/funcoesImportantes.js
@@ -0,0 +1,36 @@
+const pessoa = {
+    nome: "Rebeca",
+    idade: 2,
+    peso: 13
+}
+
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+console.log(Object.entries(pessoa));
+
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`);
+})
+
+Object.defineProperty(pessoa, "dataNascimento", {
+    enumerable: false,
+    writable: false,
+    value: "01/01/2019"
+})
+
+pessoa.dataNascimento = "01/01/2017";
+console.log(pessoa.dataNascimento);
+console.log(Object.keys(pessoa));
+
+// Object.assign (ECMAScritp 2015)
+const dest = { a: 1 };
+const o1 = { b: 2 };
+const o2 = { c: 3, a: 4 };
+const obj = Object.assign(dest, o1, o2);
+
+console.log(dest);
+console.log(obj);
+
+Object.freeze(obj);
+obj.c = 1234;
+console.log(obj);
