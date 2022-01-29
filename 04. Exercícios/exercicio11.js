fa66e8d601e9ed3904126bd6e41add401b0547c7
Diego Maciel <dcunhamaciel@gmail.com>
2022-01-29 10:14:44 -0300

    Exercício 11

---
+++ 04. Exercícios/exercicio11.js
@@ -0,0 +1,27 @@
+// As regras para o cálculo dos anos bissextos são as seguintes:
+// De 4 em 4 anos é ano bissexto;
+// De 100 em 100 anos não é ano bissexto;
+// De 400 em 400 anos é ano bissexto;
+// Prevalecem as últimas regras sobre as primeiras.
+// Partindo daí elabore uma função que recebe um ano e calcula se ele é ano bissexto, imprimindo no console a
+// mensagem e retornando true ou false.
+
+function anoBissexto(ano) {
+    if (ano <= 0) {
+        return false;
+    } else if (ano % 400 == 0) {
+        return true
+    } else if (ano % 4 == 0 && !ano % 100 == 0) {
+        return true
+    } else {
+        return false;
+    }
+}
+
+console.log(anoBissexto(0));
+console.log(anoBissexto(4));
+console.log(anoBissexto(100));
+console.log(anoBissexto(400));
+console.log(anoBissexto(800));
+console.log(anoBissexto(2020));
+console.log(anoBissexto(2021));
