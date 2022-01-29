16e240854b87e23fa9c93212d94770f99e539b61
Diego Maciel <dcunhamaciel@gmail.com>
2022-01-29 07:10:51 -0300

    Getters/Setters

---
+++ 05. Objeto/getterSetter.js
@@ -0,0 +1,17 @@
+const sequencia  = {
+    _valor: 1, // convenção
+    get valor() {
+        return this._valor++;
+    },
+    set valor(valor) {
+        if (valor > this.valor) {
+            this._valor = valor;
+        }
+    }
+}
+
+console.log(sequencia.valor, sequencia.valor);
+sequencia.valor = 1000;
+console.log(sequencia.valor, sequencia.valor);
+sequencia.valor = 900;
+console.log(sequencia.valor, sequencia.valor);
