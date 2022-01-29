c8c32a256437f7189bf75f0d2df22175291e6a75
Diego Maciel <dcunhamaciel@gmail.com>
2022-01-29 11:56:06 -0300

    Simulando Array com Objeto

---
+++ 06. Array/simulandoArray.js
@@ -0,0 +1,20 @@
+const quaseArray = {
+    0: "Rafael",
+    1: "Ana",
+    2: "Bia"
+}
+console.log(quaseArray);
+
+Object.defineProperty(quaseArray, "toString", {
+    value: function() {
+        return Object.values(this);
+    },
+    enumerable: false
+});
+console.log(quaseArray[0]);
+
+const meuArray = ["Rafael", "Ana", "Bia"];
+console.log(meuArray[0]);
+
+console.log(quaseArray.toString());
+console.log(meuArray);
