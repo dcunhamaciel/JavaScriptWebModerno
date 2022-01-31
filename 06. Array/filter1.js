b549e988483e5d93bf90763704ec417e243d9090
Diego Maciel <dcunhamaciel@gmail.com>
2022-01-31 10:48:25 -0300

    Filter

---
+++ 06. Array/filter1.js
@@ -0,0 +1,11 @@
+const produtos = [
+    { nome: 'Notebook', preco: 2499, fragil: true },
+    { nome: 'iPad Pro', preco: 4199, fragil: true },
+    { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+    { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
+]
+
+const caros = p => p.preco >= 500;
+const frageis = p => p.fragil;
+
+console.log(produtos.filter(caros).filter(frageis));
