3b39fa65a4a6bc115ac306cbd2a6651f837800b7
Diego Maciel <dcunhamaciel@gmail.com>
2022-01-31 14:40:11 -0300

    Concat

---
+++ 06. Array/concat.js
@@ -0,0 +1,9 @@
+const filhas = ['Valeskah', 'Cibalena'];
+const filhos = ['Uoxiton', 'Uesclei'];
+const total = filhas.concat(filhos, 'Fulano');
+
+console.log(filhas);
+console.log(filhos);
+console.log(total);
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]));
