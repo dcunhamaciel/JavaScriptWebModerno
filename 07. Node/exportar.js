85cbfdc6e8e860ada79482b6eaf86efe1b66c5a4
Diego Maciel <dcunhamaciel@gmail.com>
2022-02-01 11:12:13 -0300

    Sistema de Módulos: Exports

---
+++ 07. Node/exportar.js
@@ -0,0 +1,17 @@
+console.log(module.exports === this);
+console.log(module.exports === exports);
+
+this.a = 1;
+exports.b = 2;
+module.exports.c = 3;
+
+exports = null; // apenas "desliga" a referência de module.exports
+console.log(module.exports);
+
+exports = {
+    nome: 'Teste'
+}
+
+console.log(module.exports);
+
+module.exports = { publico: true };
