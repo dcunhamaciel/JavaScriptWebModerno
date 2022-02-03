ab8b976c307b33da39d83e55bed1ae843c67d1c7
Diego Maciel <dcunhamaciel@gmail.com>
2022-02-03 11:32:01 -0300

    API com Express

--- 07. Node/projeto/src/servidor.js
+++ 07. Node/projeto/src/servidor.js
@@ -3,9 +3,39 @@ const porta = 3003;
 const { response } = require('express');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const bancoDeDados = require('./bancoDeDados');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/produtos', (request, response, next) => {
-    response.send({ nome: 'Notebook', preco: 123.45 }); // converter para JSON
+    response.send(bancoDeDados.getProdutos());
+});
+
+app.get('/produtos/:id', (request, response, next) => {
+    response.send(bancoDeDados.getProduto(request.params.id));
+});
+
+app.post('/produtos', (request, response, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: request.body.nome,
+        preco: request.body.preco
+    });
+    response.send(produto); // JSON
+});
+
+app.put('/produtos/?:id', (request, response, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: request.body.id,
+        nome: request.body.nome,
+        preco: request.body.preco
+    });
+    response.send(produto); // JSON
+});
+
+app.delete('/produtos/?:id', (request, response, next) => {
+    const produto = bancoDeDados.excluirProduto(request.body.id);
+    response.send(produto); // JSON
 });
 
 app.listen(porta, () => {
