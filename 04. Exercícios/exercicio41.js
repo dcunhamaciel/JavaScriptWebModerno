26d8f4154c3642551ededd341c01592766b1ec4a
Diego Maciel <dcunhamaciel@gmail.com>
2022-02-09 18:38:38 -0300

    Exercício 41

---
+++ 04. Exercícios/exercicio41.js
@@ -0,0 +1,9 @@
+// Crie uma função que retorna a string "Olá, " concatenada com um argumento text 
+// (a ser passado para a função) e com ponto de exclamação "!" no final.
+
+function cumprimentar(nome) {
+    return `Olá, ${nome}!`;
+}
+
+console.log(cumprimentar("Leonardo"));
+console.log(cumprimentar("Maria"));
