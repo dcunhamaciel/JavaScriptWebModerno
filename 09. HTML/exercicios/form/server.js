9e35492865c6c13ad3e697068e5c25d84555f7df
Diego Maciel <dcunhamaciel@gmail.com>
2022-02-12 15:45:45 -0300

    Formulário

--- 09. HTML/exercicios/form/server.js
+++ 09. HTML/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/usuarios', (request, response) => {
     console.log(request.body);
-    response.send('<h1>Parabéns</h1>');
+    response.send('<h1>Parabéns. Usuário Incluído!!</h1>');
+});
+
+app.post('/usuarios/:id', (request, response) => {
+    console.log(request.body);
+    console.log(request.params.id);
+    response.send('<h1>Parabéns. Usuário Alterado!!</h1>');
 });
 
 app.listen(3003);
