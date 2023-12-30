93c39a8cc036ba6b4da0c4bf7e32ae1fa4d1786a
dcunhamaciel <dcunhamaciel@gmail.com>
2023-12-30 12:22:23 -0300

    Flappy Bird 05

--- 11. HTML + CSS + JS/js/flappy.js
+++ 11. HTML + CSS + JS/js/flappy.js
@@ -98,3 +98,39 @@ function Passaro(alturaJogo) {
 
     this.setY(alturaJogo / 2)
 }
+
+function Progresso() {
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+}
+
+function FlappyBird() {
+    let pontos = 0
+
+    const areaDoJogo = document.querySelector('[wm-flappy]')
+    const altura = areaDoJogo.clientHeight
+    const largura = areaDoJogo.clientWidth
+
+    const progresso = new Progresso()
+    const barreiras = new Barreiras(altura, largura, 200, 400, 
+        () => progresso.atualizarPontos(++pontos))
+    const passaro = new Passaro(altura)    
+
+    areaDoJogo.appendChild(progresso.elemento)
+    areaDoJogo.appendChild(passaro.elemento)
+    
+    barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+    this.start = () => {
+        // loop do jogo
+        const temporizador = setInterval(() => {
+            barreiras.animar()
+            passaro.animar()
+        }, 20)
+    }
+}
+
+new FlappyBird().start()
