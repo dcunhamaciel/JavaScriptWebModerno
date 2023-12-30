15aa05ce84bd97bd6484be6d14917d306d78c41c
dcunhamaciel <dcunhamaciel@gmail.com>
2023-12-30 12:14:30 -0300

    Flappy Bird 04

--- 11. HTML + CSS + JS/js/flappy.js
+++ 11. HTML + CSS + JS/js/flappy.js
@@ -70,3 +70,31 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
         })
     }
 }
+
+function Passaro(alturaJogo) {
+    let voando = false
+
+    this.elemento = novoElemento('img', 'passaro')
+    this.elemento.src = 'imgs/passaro.png'
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+
+    window.onkeydown = e => voando = true
+    window.onkeyup = e => voando = false
+
+    this.animar = () => {
+        const novoY = this.getY() + (voando ? 8 : -5)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+
+        if (novoY <= 0) {
+            this.setY(0)
+        } else if (novoY >= alturaMaxima) {
+            this.setY(alturaMaxima)
+        } else {
+            this.setY(novoY)
+        }
+    }
+
+    this.setY(alturaJogo / 2)
+}
