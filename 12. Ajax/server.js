e79657c5ec9f0d2b1ea613087525447c3c09f208
dcunhamaciel <dcunhamaciel@gmail.com>
2024-01-08 21:22:05 -0300

    XMLHttpRequest #01

--- 12. Ajax/server.js
+++ 12. Ajax/server.js
@@ -8,5 +8,4 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
