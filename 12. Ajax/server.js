5529751ed32c6154c7323a55d93740a5c02c6198
dcunhamaciel <dcunhamaciel@gmail.com>
2025-01-11 09:26:29 -0300

    Axios #02

--- 12. Ajax/server.js
+++ 12. Ajax/server.js
@@ -37,4 +37,11 @@ app.post('/formulario', (req, res) => {
     })
 })
 
+app.get('/parOuImpar', (req, res) => {
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
